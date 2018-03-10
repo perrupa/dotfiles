f1d82d909406df6ca232b3ee2e122cc68b005c6e
Chris Marlow <christopher.marlow@shopify.com>
2018-03-10 16:21:06 -0500

    adds git aliases in zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,9 @@
 ## Aliases
-alias gco='git checkout $(git branch | grep -v "\*" | fzf)'
+alias e='nvim'
 alias vim='nvim'
-alias gits='git'
 
+alias gs='git st'
+alias ga='git a'
+alias gc='git commit'
+alias gco='git checkout $(git branch | grep -v "\*" | fzf)'
 

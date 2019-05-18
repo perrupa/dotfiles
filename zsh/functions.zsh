fff90df9f2d8593cd354b7b8331b6b43cd48f16e
Chris Marlow <christopher.marlow@shopify.com>
2019-05-18 15:24:35 -0400

    Improving tmux function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,10 +9,11 @@ t () {
 # tmux session creation/loading with FZF
 tm() {
   local session
-  newsession=${1:-new}
+  newsession=${1:-Personal}
   session=$(tmux list-sessions -F "#{session_name}" | \
     fzf --query="$1" --select-1 --exit-0) &&
-    tmux attach-session -t "$session" || tmux new-session -s $newsession
+    tmux attach-session -t "$session" \
+    || tmux new-session -s $newsession -c ~/Projects
 }
 
 function diff {

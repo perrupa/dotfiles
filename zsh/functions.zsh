dcbe425311fc78bea48497574bf3c1e941e6ce13
Chris Marlow <christopher.marlow@shopify.com>
2019-12-29 02:15:12 -0500

    Improved default tmux session # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is ahead of 'origin/master' by 2 commits. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   zsh/functions.zsh # # Untracked files: #	dirs -v #	kitty/ #

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,16 +6,6 @@ t () {
     noglob bundle exec rake test "$@"
 }
 
-# tmux session creation/loading with FZF
-tm() {
-  local session
-  newsession=${1:-Personal}
-  session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf --query="$1" --select-1 --exit-0) &&
-    tmux attach-session -t "$session" \
-    || tmux new-session -s $newsession -c ~/Projects
-}
-
 function diff {
   colordiff -u "$@" | less
 }
@@ -27,5 +17,25 @@ function fkill() {
   then
     kill -${1:-9} $pid
   fi
+}
+
+# Tmux section
+function create_personal_tmux_session() {
+  SESSION_NAME="Personal"
+  tmux new-session -d -s $SESSION_NAME -c ~/Projects/dotfiles -n 'Dotfiles'
+  tmux new-window -t $SESSION_NAME: -n 'Notes'
+  tmux send-keys 'cd ~/Notes' Enter
+  tmux send-keys "clear" Enter
+  tmux select-window -t $SESSION_NAME:1
+}
+
+# tmux session creation/loading with FZF
+tm() {
+  local session
+  newsession=${1:-Personal}
+  session=$(tmux list-sessions -F "#{session_name}" | \
+    fzf --query="$1" --select-1 --exit-0) &&
+    tmux attach-session -t "$session" \
+    || $( create_personal_tmux_session && tmux attach-session -t "Personal")
 }
 

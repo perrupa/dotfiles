d005246230c00fa8979708e854f9d86d2b6ea314
Chris Marlow <christopher.marlow@shopify.com>
2020-03-12 13:18:32 -0400

    Fixes my npm wonkiness

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,21 +1,7 @@
 # Source npm
 export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR"/nvm.sh --no-use
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR"/nvm.sh
 
-# declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-
-# NODE_GLOBALS+=("node")
-# NODE_GLOBALS+=("nvm")
-# NODE_GLOBALS+=("yarn")
-# NODE_GLOBALS+=("gatsby")
-
-# load_nvm () {
-#     export NVM_DIR=~/.nvm
-#     [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-# }
-
-# for cmd in "${NODE_GLOBALS[@]}"; do
-#     eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
-# done
-
+# Lazy load nvm
+# e.g. '--no-use' === don't call `nvm use default`
+# [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR"/nvm.sh --no-use

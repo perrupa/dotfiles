9145065b246aaef79210c974433b440164571355
Chris Marlow <christopher.marlow@shopify.com>
2022-01-05 13:06:59 -0500

    Trimming zshrc

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,6 +1,8 @@
 # Source npm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR"/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 # Lazy load nvm
 # e.g. '--no-use' === don't call `nvm use default`

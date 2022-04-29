18daebd84c4c00227182394c245eb50b8f43123b
Chris Marlow <christopher.marlow@shopify.com>
2022-04-29 08:51:49 -0400

    Fixing tmux install script to add a folder symlink
    Without the slash the symlink didn't think the symlink was to a folder

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,4 +4,4 @@ CURR_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "Script location: ${CURR_DIR}"
 
 ln -fs $CURR_DIR/tmux.conf  ~/.tmux.conf
-ln -fs $CURR_DIR ~/.config/tmux
+ln -fs $CURR_DIR/ ~/.config/tmux

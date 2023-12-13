0162d751fc7fdf6524e2b7a666b68b6170280f7d
Chris Marlow <christopher.marlow@shopify.com>
2023-12-13 10:39:20 -0500

    Adding code-action alias

--- nvim/init.lua
+++ nvim/init.lua
@@ -254,6 +254,7 @@ vim.keymap.set('n', ';sh', require('telescope.builtin').help_tags, { desc = '[S]
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', ';sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', ';sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', ';a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { desc = 'Code [A]ctions' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`

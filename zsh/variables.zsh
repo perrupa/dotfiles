588322e6412fce5b68cc8c15bff01a1fd893370f
Chris Marlow <christopher.marlow@shopify.com>
2025-01-02 12:25:24 -0500

    zsh history file + ctrl(+shift)+tab tmux alias

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -6,3 +6,9 @@ export EDITOR='nvim'
 
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git/*'"
 export NVM_LAZY_LOAD=true
+
+HISTFILE=~/.zsh_history
+HISTSIZE=100000
+SAVEHIST=100000
+setopt appendhistory
+

19d3d773754000c6eba4ac7be97b423f50cfb113
Chris Marlow <christopher.marlow@shopify.com>
2025-08-13 23:35:46 -0400

    removing shopifisms

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,5 @@ echo "Bootstrapping..."
 sh ./git/install.sh
 sh ./vim/install.sh
 sh ./nvim/install.sh
-
-if [ "$SPIN" ]; then
-  # Spin specific setup
-  sh spin-bootstrap.sh
-else
-  # Non-spin setup
-  sh ./zsh/install.sh
-  touch ~/this-is-not-spin.txt
-fi
+sh ./zsh/install.sh
 

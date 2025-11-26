b675d05345df1699761f3dbb7b65c6e7d0013e04
Chris Marlow <christopher.marlow@shopify.com>
2025-11-26 11:18:36 -0500

    Updating date in Sketchybar

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -4,5 +4,5 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %I:%M %p')"
+sketchybar --set "$NAME" label="$(date '+%b %m — %I:%M %p')"
 

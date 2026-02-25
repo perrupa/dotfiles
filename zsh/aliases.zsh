2d2f5fd8837f29d0421ca0c0b442d5aa644b5ebd
Chris Marlow <christopher.marlow@shopify.com>
2026-02-25 14:23:23 -0500

    Adding ls aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,16 @@
 alias e='nvim'
 alias vim='nvim'
 
-# alias ls=colorls
+alias la='ls -al'
+alias ll='ls -l'
+# If `eza` is available, alias it.
+if command -v eza >/dev/null 2>&1; then
+  LS_COMMAND="eza --group-directories-first -F "
+  alias ls="$LS_COMMAND"
+  alias la="$LS_COMMAND -al"
+  alias ll="$LS_COMMAND -l"
+  alias l="$LS_COMMAND -lbF --git"
+fi
 
 alias gs='git st'
 alias ga='git a'
